92f8d52cca0bf942e818f686127c41bb5870b487
Atlas <btw210899@gmail.com>
2023-01-06 16:54:33 +0200

    feat: added test cron command

--- apps/api/src/app/controllers/user.controller.ts
+++ apps/api/src/app/controllers/user.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Post, UseGuards } from '@nestjs/common'
+import { Controller, Logger, Post, UseGuards } from '@nestjs/common'
+import { Cron } from '@nestjs/schedule'
 import { JWTAuthGuard } from '../guards/jwt.guard'
 import { UserID } from '../guards/user.decorator'
 
@@ -8,5 +9,10 @@ export class UserController {
 	@Post('info')
 	async info(@UserID() userId: string) {
 		console.log(userId)
+	}
+
+	@Cron('*/5 * * * * *')
+	async cron() {
+		Logger.log('Done')
 	}
 }
